65a35047603d725b857261e5edafe1fc48127f90
avinash.kemparaju <avinash.kemparaju@freshworks.com>
2021-06-28 14:04:29 +0530

    rails commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   # makes methods to be used in both views & other controllers
-  helper_method :current_user, :signed_in?
+  helper_method :current_user, :signed_in?, :article_count
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
@@ -12,7 +12,11 @@ class ApplicationController < ActionController::Base
     !!current_user
   end
 
-  # can be used in views
+  def article_count
+    Article.count
+  end
+
+  # cannot be used in views
   def require_user
     if !signed_in?
       flash[:alert] = 'You must be logged in'
