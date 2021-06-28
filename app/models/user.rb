7e2b9349d7358229fb717bd2dd98e88f5609c172
avinash.kemparaju <avinash.kemparaju@freshworks.com>
2021-06-28 08:59:03 +0530

    create user module added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,17 @@
 class User < ActiveRecord::Base
-  has_many :articles
+  has_many :articles, dependent: :destroy
   attr_accessible :email, :username, :password
+
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,
+                    uniqueness: { case_sensitive: false },
+                    length: { maximum: 105 },
+                    format: { with: EMAIL_REGEX }
+
+  validates :username, presence: true,
+                      uniqueness: { case_sensitive: false },
+                      length: { minimum: 3, maximum: 25 }
+
+  before_save { self.email = email.downcase }
   has_secure_password
 end
