edd175682b89814de298a99008050c0da6bc96d7
avinash.kemparaju <avinash.kemparaju@freshworks.com>
2021-06-28 16:05:00 +0530

    rails commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_filter :set_user, only: [:show, :edit, :update]
 
   def new
     @user = User.new
@@ -19,6 +20,22 @@ class UsersController < ApplicationController
   end
 
   def show
+  end
+
+  def edit
+  end
+
+
+  def update
+    if @user.update_attributes(params[:user])
+      flash['notice'] = 'Update successful'
+      redirect_to @user
+    end
+  end
+
+  private
+
+  def set_user
     @user = User.find(params[:id])
   end
 end
