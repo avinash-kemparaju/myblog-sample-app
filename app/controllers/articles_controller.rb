9dc7e19560f91d29bf487792fbb9890af44165ce
avinash.kemparaju <avinash.kemparaju@freshworks.com>
2021-06-28 13:39:12 +0530

    rails commit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
+  before_filter :set_article, only: [:show, :edit, :update, :destroy]
+  before_filter :require_user, except: [:show, :index]
+  before_filter :require_correct_user, only: [:edit, :update, :destroy]
+
   def index
   end
 
@@ -7,5 +11,29 @@ class ArticlesController < ApplicationController
   end
 
   def create
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+    @article.destroy
+    flash[:notice] = "Article #{@article.id} deleted successfully"
+    redirect_to articles_path
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def require_correct_user
   end
 end
