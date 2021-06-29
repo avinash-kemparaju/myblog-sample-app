253968b7c78e3f0154440000680162d3999e06fe
avinash.kemparaju <avinash.kemparaju@freshworks.com>
2021-06-29 23:10:49 +0530

    integration test added for reject new category cases

--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -14,4 +14,25 @@ class CreateCategoryTest < ActionDispatch::IntegrationTest
     assert_match _name, response.body
   end
 
+
+  test 'get new category form and reject empty category' do
+    _scenarios = [
+      { name: '', error: 'Name can&#x27;t be blank' },
+      { name: 'aa', error: 'Name is too short (minimum is 3 characters)' },
+      { name: 'a' * 100, error: 'Name is too long (maximum is 30 characters)' }
+    ]
+
+    _scenarios.each do |_case|
+      get 'categories/new'
+      assert_response :success
+      assert_no_difference 'Category.count' do
+        post categories_path, category: { name: _case[:name] }
+      end
+      assert_match 'errors', response.body
+      assert_select 'div.alert'
+      assert_select 'h5.alert-heading'
+      assert_match _case[:error], response.body
+    end
+  end
+
 end
