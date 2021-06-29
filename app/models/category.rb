f4d6662c7c80309c286895b3d35135fa8217e58e
avinash.kemparaju <avinash.kemparaju@freshworks.com>
2021-06-29 20:19:38 +0530

    added category model test cases

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ActiveRecord::Base
   attr_accessible :name
+  validates :name, presence: true, length: { maximum: 30, minimum: 3 }
+  validates_uniqueness_of :name
 end
--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -2,11 +2,33 @@ require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
 
-  test 'category should be valid' do
+  def setup
     @category = Category.new(name: 'Sports')
+  end
+
+  test 'category should be valid' do
     assert @category.valid?
   end
 
+  test 'name should be present' do
+    @category.name = ''
+    assert !@category.valid?
+  end
 
+  test 'name should be unique' do
+    @category.save
+    @category2 = Category.new(name: 'Sports')
+    assert !@category2.valid?
+  end
+
+  test 'name should not be too long' do
+    @category.name = 'CA'
+    assert !@category.valid?
+  end
+
+  test 'name should not be too short' do
+    @category.name = 'A' * 100
+    assert !@category.valid?
+  end
 
 end
